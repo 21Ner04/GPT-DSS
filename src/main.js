ed8634ebef773fc34fdd67ffcddb5758bfdcb360
AndreyPiganov <andrey.piganov@mail.ru>
2023-06-12 12:19:29 +0300

    Сделаны абстрактные работы кнопок(чтобы было видно, что они рабочие)

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
-const buttonTest = document.querySelector('.submit');
-buttonTest.addEventListener('click', (event) =>{
-    alert('hello')
+const state = 
+{
+
+}
+const submitButton = document.querySelector('.submit');
+const addButton = document.querySelector('.add-chat');
+const regenerateButton = document.querySelector('.response');
+submitButton.addEventListener('click', (event) =>{
+    alert('я отправляю запрос')
+    console.log(event.target)
+})
+addButton.addEventListener('click', (event) =>{
+    alert('Я добавляю чат')
+    console.log(event.target)
+})
+regenerateButton.addEventListener('click', (event) =>{
+    alert('Переделываю запрос')
+    console.log(event.target);
 })
