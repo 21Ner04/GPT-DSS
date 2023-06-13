5c9df17154edb8c0c93962de81b75f2a7b20a312
AndreyPiganov <andrey.piganov@mail.ru>
2023-06-13 19:43:01 +0300

    install openai

--- __tests__/test.test.js
+++ __tests__/test.test.js
@@ -1,5 +1,8 @@
+import rumPrompt from '../src/libs/getApi.js'
+
 // eslint-disable-next-line no-undef
 test('test', () => {
   // eslint-disable-next-line no-undef
   expect('to').toEqual('to');
+  expect(rumPrompt()).toEqual('man')
 });
