75b54e2f4323f3089059460c65d1efb1aa6bdaaa
AndreyPiganov <andrey.piganov@mail.ru>
2023-06-14 16:45:36 +0300

    eslint fix

--- src/main.js
+++ src/main.js
@@ -1,52 +1,55 @@
 // eslint-disable-next-line no-unused-vars
-const state = 
-{
+const state = {
   lang: 'en',
 };
-const body = document.body
+const { body } = document;
 const submitButton = document.querySelector('.btn-submit');
 const addButton = document.querySelector('.add-chat');
 const regenerateButton = document.querySelector('.response');
 
 // dark - light mode knopka
-const toggle = document.querySelector('.theme')
-toggle.addEventListener('click', (event) =>{
-  const elementSection = document.querySelector('.main').querySelectorAll('*')
-  if(event.target.classList.contains('dark-mode')){
-    body.classList.remove('body-black')
-    body.classList.add('body-white')  
-    event.target.classList.remove('dark-mode');
-    event.target.classList.add('light-mode');
-    event.target.textContent = '☾ Dark mode'
-    elementSection.forEach((element) =>{
-      element.style.color = 'black';
-    })
-  }else{
-    event.target.classList.remove('light-mode');
-    event.target.classList.add('dark-mode');
-    body.classList.remove('body-white') 
-    body.classList.add('body-black') 
-    event.target.textContent = '☼ Light mode';
-    elementSection.forEach((element) =>{
-      element.style.color = 'white';
-    })
+const toggle = document.querySelector('.theme');
+toggle.addEventListener('click', (event) => {
+  const elementSection = document.querySelector('.main').querySelectorAll('*');
+  const myEvent = event;
+  if (myEvent.target.classList.contains('dark-mode')) {
+    body.classList.remove('body-black');
+    body.classList.add('body-white');
+    myEvent.target.classList.remove('dark-mode');
+    myEvent.target.classList.add('light-mode');
+    myEvent.target.textContent = '☾ Dark mode';
+    elementSection.forEach((element) => {
+      const copyElement = element;
+      copyElement.style.color = 'black';
+    });
+  } else {
+    myEvent.target.classList.remove('light-mode');
+    myEvent.target.classList.add('dark-mode');
+    body.classList.remove('body-white');
+    body.classList.add('body-black');
+    myEvent.target.textContent = '☼ Light mode';
+    elementSection.forEach((element) => {
+      const copyElement = element;
+      copyElement.style.color = 'white';
+    });
   }
-})
+});
 // -----------------------------------------------------
 
 // re - e flag knopka
-const language = document.querySelector(".theme-toggle")
-language.addEventListener('click', (event) =>{
-  if(state.lang === 'en'){
-  event.target.src = 'assets/favicon/favicon-ru-32x32.png';
-  event.alt = 'ru';
-  state.lang = 'ru';
-}else{
-  event.target.src = 'assets/favicon/favicon-e-32x32.png';
-  event.alt = 'en';
-  state.lang = 'en';
-}
-})
+const language = document.querySelector('.theme-toggle');
+language.addEventListener('click', (event) => {
+  const myEvent = event;
+  if (state.lang === 'en') {
+    myEvent.target.src = 'assets/favicon/favicon-ru-32x32.png';
+    myEvent.alt = 'ru';
+    state.lang = 'ru';
+  } else {
+    myEvent.target.src = 'assets/favicon/favicon-e-32x32.png';
+    myEvent.alt = 'en';
+    state.lang = 'en';
+  }
+});
 // -----------------------------------------------------
 
 submitButton.addEventListener('click', () => {
--- src/server.js
+++ src/server.js
@@ -3,5 +3,3 @@ const tester = () => {
   return response + 1;
 };
 export default tester;
-//<button class="theme-toggle" disabled><p> <img src="assets/favicon/favicon-ru-32x32.png" alt="ru" width="25px" height="25px">  <img src="assets/favicon/favicon-e-32x32.png" alt="en" width="25px" height="25px"></p></button>
-
