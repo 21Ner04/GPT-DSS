c8f26293ee8c18b60fcd2632292a4b308ff42fe0
AndreyPiganov <andrey.piganov@mail.ru>
2023-06-14 14:02:39 +0300

    toggle button

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ const state = {};
 const submitButton = document.querySelector('.btn-submit');
 const addButton = document.querySelector('.add-chat');
 const regenerateButton = document.querySelector('.response');
+const toggle = document.querySelector('.toggle');
+toggle.addEventListener('click', (event) =>{
+  const button = document.createElement('button');
+  button.classList.add('toggle');
+  
+})
 submitButton.addEventListener('click', () => {
   // eslint-disable-next-line no-alert
   alert('я отправляю запрос');
