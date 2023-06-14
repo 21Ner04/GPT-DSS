197e37e925b3883e6edbbc299908f4094f901c99
AndreyPiganov <andrey.piganov@mail.ru>
2023-06-14 13:27:44 +0300

    update server.js

--- src/server.js
+++ src/server.js
@@ -1,27 +1,5 @@
-import { Configuration, OpenAIApi } from 'openai';
-
-const config = new Configuration({
-  apiKey: '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',
-});
-
-const openai = new OpenAIApi(config);
-
-const runPrompt = async () => {
-  const prompt = 'Tell me a joke about a cat eating pasta';
-
-  const response = await openai.createCompletion({
-    model: 'gpt 3.5 turbo',
-    prompt,
-    max_tokens: 2048,
-    temperature: 1,
-  });
-
-  const parsableJSONresponse = response.data.choices[0].text;
-  return parsableJSONresponse;
-};
 const tester = () => {
   const response = 0;
   return response + 1;
 };
-runPrompt();
 export default tester;
