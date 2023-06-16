54a4f951fd00a7da23a48e1dec13ba31fcc11fc8
21Ner04 <ttobievna@yandex.ru>
2023-06-16 16:44:46 +0200

    bot

--- src/init.js
+++ src/init.js
@@ -96,14 +96,15 @@ const app = async () => {
   // eslint-disable-next-line no-alert
     alert('я отправляю запрос');
   });
-  addButton.addEventListener('click', (event) => {
+  addButton.addEventListener('click', () => {
     const ol = document.querySelector('.chat-list > ol');
     const li = document.createElement('li');
     const a = document.createElement('a');
-    a.textContent = 'New chat'
-    a.classList.add('btn')
+    a.textContent = 'New chat';
+    a.classList.add('btn');
     li.appendChild(a);
     ol.appendChild(li);
+    // eslint-disable-next-line no-alert
     alert('Я добавляю чат');
   });
   regenerateButton.addEventListener('click', () => {
