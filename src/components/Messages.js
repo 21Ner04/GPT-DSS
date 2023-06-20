99815f97982517be0a3e150e42b165eb1492a890
21Ner04 <ttobievna@yandex.ru>
2023-06-20 13:00:12 +0200

    update

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,20 +1,25 @@
 export default class Messages {
-    constructor(){
-        this.messages = [{role: 'system',content: 'You-user',},];
-    }
-    add(role, content){
-        this.messages.push({role: role, content: content});
-    }
-    getItems(){
-        return this.messages;
-    }
-    removeLast(){
-        this.messages.pop();
-    }
-    removeFirst(){
-        this.messages.shift();
-    }
-    reset(){
-        this.messages = [];
-    }
+  constructor() {
+    this.messages = [{ role: 'system', content: 'You-user' }];
+  }
+
+  add(role, content) {
+    this.messages.push({ role, content });
+  }
+
+  getItems() {
+    return this.messages;
+  }
+
+  removeLast() {
+    this.messages.pop();
+  }
+
+  removeFirst() {
+    this.messages.shift();
+  }
+
+  reset() {
+    this.messages = [];
+  }
 }
